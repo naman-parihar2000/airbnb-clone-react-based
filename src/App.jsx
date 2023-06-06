4abc3b015c359360addb2877f83201b0145257b4
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-06 23:24:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route } from 'react-router'
+import { Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './Layout'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout></Layout>}>
+        <Route index element={<IndexPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Route>
+    </Routes>
+   
+  )
+}
+
+export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+    return (
+        <div>
+            <form>
+                <input type='email'></input>
+            </form>
+        </div>
+    )
+}
